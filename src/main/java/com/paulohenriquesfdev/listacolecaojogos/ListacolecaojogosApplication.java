712606b82155825d5133b0c213023464f9119145
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-07-13 03:25:02 -0300

    Project Created

---
+++ src/main/java/com/paulohenriquesfdev/listacolecaojogos/ListacolecaojogosApplication.java
@@ -0,0 +1,13 @@
+package com.paulohenriquesfdev.listacolecaojogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListacolecaojogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListacolecaojogosApplication.class, args);
+	}
+
+}
