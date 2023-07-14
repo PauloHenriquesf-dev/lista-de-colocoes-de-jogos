de1e90d9c91618e507311e91f938d2fb645262e5
PauloHenriquesf-dev <paulohenriquesf97@gmail.com>
2023-07-14 13:31:14 -0300

    GET game by id

--- src/main/java/com/paulohenriquesfdev/listacolecaojogos/controllers/GameController.java
+++ src/main/java/com/paulohenriquesfdev/listacolecaojogos/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paulohenriquesfdev.listacolecaojogos.dto.GameDTO;
 import com.paulohenriquesfdev.listacolecaojogos.dto.GameMinDTO;
 import com.paulohenriquesfdev.listacolecaojogos.services.GameService;
 
@@ -17,6 +19,13 @@ public class GameController {
 	//injetando uma instancia do GameService pra ser possivel o GameController chamar o GameService
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+		// chama o servise que chama o repository do banco de dados, retorna um GameDTO e retorna o result.
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
--- src/main/java/com/paulohenriquesfdev/listacolecaojogos/services/GameService.java
+++ src/main/java/com/paulohenriquesfdev/listacolecaojogos/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.paulohenriquesfdev.listacolecaojogos.dto.GameDTO;
 import com.paulohenriquesfdev.listacolecaojogos.dto.GameMinDTO;
 import com.paulohenriquesfdev.listacolecaojogos.entities.Game;
 import com.paulohenriquesfdev.listacolecaojogos.repositories.GameRepository;
@@ -16,7 +18,21 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		// recebe um id e retorna um objeto
+		// findById retorna tipo Optional, pra pegar o dado, o game q ta dentro desse Optional, usa o .get
+		Game result = gameRepository.findById(id).get();
+		// se o id nao existir pode fazer um tratamento (aqui nao vamos fazer)
+		
+		//converter Game para GameDTO:
+		GameDTO dto = new GameDTO(result); // construtor de GameDTO
+		return dto;
+	}
+	
+	
 	// funcao que retorna uma lista de games DTO
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		
 		/* trazer do banco de dados uma lista de games (todos - findAll() ):
